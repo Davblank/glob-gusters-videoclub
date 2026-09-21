240511923d2c41294fd52dbf350ab768637c7fb1
Davblank <ufueufueosas11@gmail.com>
2026-09-21 17:03:13 -0500

    feat: Add SQL script for database and table creation

--- sql/glob_gusters.sql
+++ sql/glob_gusters.sql
@@ -3,3 +3,22 @@
 -- Fecha: 2026-09-15
 -- Yo aqui estoy
 
+-- Creación de la base de datos
+CREATE DATABASE IF NOT EXISTS `glob_gusters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `glob_gusters`;
+
+-- Eliminación de la base de datos
+-- DROP DATABASE IF EXISTS `glob_gusters`;
+
+CREATE TABLE IF NOT EXISTS Nacionalidad (
+    NacionalidadID int PRIMARY KEY,
+    Nombre varchar(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Actor (
+    ActorID int PRIMARY KEY,
+    Nombre varchar(100) NOT NULL,
+    Sexo varchar(10) NOT NULL,
+    NacionalidadID int,
+    FOREIGN KEY (NacionalidadID) REFERENCES Nacionalidad(NacionalidadID)
+);
